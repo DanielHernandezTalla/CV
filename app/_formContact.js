58204c15318cf175a156c72e6cae2fd69f73d688
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-04-19 17:28:24 -0700

    Add function to form contact

---
+++ app/_formContact.js
@@ -0,0 +1,52 @@
+'use strict'
+export default function formContact() {
+    const $formContacto = document.getElementById("contact-mail"),
+        $required = document.querySelectorAll("[required]")
+
+    // Aqui creamos dinamicamente los span que aconpanan a los inputs para los errores
+    $required.forEach(input => {
+        const $span = document.createElement("span")
+
+        $span.classList.add("contact-mail__error")
+        $span.classList.add("contact-mail__error-none")
+        $span.textContent = "Error: " + input.title
+        input.insertAdjacentElement("afterend", $span)
+    })
+
+    document.addEventListener("click", e => {
+        // Click en el submit del formulario
+        if (e.target === $formContacto.button) {
+
+            // Validacion del nombre
+            let pattern = $formContacto.name.pattern,
+                regex = new RegExp(pattern)
+
+            if (!regex.exec($formContacto.name.value)) {
+                $formContacto.name.nextSibling.classList.remove("contact-mail__error-none")
+                e.preventDefault()
+            } else
+                $formContacto.name.nextSibling.classList.add("contact-mail__error-none")
+
+            // Validacion del email
+            pattern = $formContacto.email.pattern
+            regex = new RegExp(pattern)
+
+            if (!regex.exec($formContacto.email.value)) {
+                $formContacto.email.nextSibling.classList.remove("contact-mail__error-none")
+                e.preventDefault()
+            } else
+                $formContacto.email.nextSibling.classList.add("contact-mail__error-none")
+
+            // Validacion messaje
+            pattern = $formContacto.message.dataset.pattern
+            regex = new RegExp(pattern)
+
+            if (!regex.exec($formContacto.message.value)) {
+                $formContacto.message.nextSibling.classList.remove("contact-mail__error-none")
+                e.preventDefault()
+            } else
+                $formContacto.message.nextSibling.classList.add("contact-mail__error-none")
+
+        }
+    })
+}
--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 'use strict'
 
+import formContact from "./_formContact.js"
 import functionNav from "./_nav.js"
 
 document.addEventListener("DOMContentLoaded", e => {
 
     functionNav()
+
+    formContact()
 
 })
