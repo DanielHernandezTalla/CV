1adccabb69bd8e8afd4585c5facd7f9ed29baf86
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2022-08-04 16:51:43 -0700

    SFC: add first theme of proyects

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 'use strict'
 
 // import formContact from "./_formContact.js"
-import functionNav from "./nav.js"
+import functionNav from "./module/nav.js"
+import scrollSpy from "./module/scrollSpy.js"
 
 document.addEventListener("DOMContentLoaded", e => {
 
     functionNav()
+
+    scrollSpy()
 
     // formContact()
 
---
+++ app/module/scrollSpy.js
@@ -0,0 +1,25 @@
+'use strict';
+
+export default function scrollSpy(){
+    
+    const $sections = document.querySelectorAll("section[data-scroll-spy]")
+
+    const cb = entries =>{
+
+        entries.forEach(entry => {
+
+            const id = entry.target.getAttribute("id")
+
+            if (entry.isIntersecting)
+                document.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.add("nav__a-selected")
+            else 
+                document.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.remove("nav__a-selected")
+        })
+    }
+
+    const observer = new IntersectionObserver(cb, {
+        threshold: [.6]
+    })
+
+    $sections.forEach(el => observer.observe(el))
+}
