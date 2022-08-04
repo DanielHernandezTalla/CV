ea847cdf9b83e990b94a9ce7ab69aa771ed47d92
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2022-08-04 08:54:19 -0700

    fix: fixing routes of icon and js documents

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
-import formContact from "./_formContact.js"
-import functionNav from "./_nav.js"
+// import formContact from "./_formContact.js"
+import functionNav from "./nav.js"
 
 document.addEventListener("DOMContentLoaded", e => {
 
